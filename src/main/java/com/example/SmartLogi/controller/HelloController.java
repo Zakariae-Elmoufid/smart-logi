e7369b1fc2da9565e1edab912b23cb8f67460fd1
zakariae elmoufid <moufidzakarie3@gmail.com>
2025-10-27 22:21:25 +0100

    first commit

---
+++ src/main/java/com/example/SmartLogi/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.example.SmartLogi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/index")
+    public String hello(){
+        return "HelloController SmartLogi!";
+    }
+}
