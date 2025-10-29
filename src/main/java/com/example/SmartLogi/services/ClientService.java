c7452b8aa5d8460c8702d05aedae75f187b1876c
zakariae elmoufid <moufidzakarie3@gmail.com>
2025-10-29 11:14:34 +0100

    SCRUM-7 Créer un compte client et compléter le profil

--- src/main/java/com/example/SmartLogi/services/ClientService.java
+++ src/main/java/com/example/SmartLogi/services/ClientService.java
@@ -6,8 +6,12 @@ import com.example.SmartLogi.entities.Client;
 import com.example.SmartLogi.enums.UserRole;
 import com.example.SmartLogi.mapper.ClientMapper;
 import com.example.SmartLogi.repositories.ClientRepository;
+import com.example.SmartLogi.util.PasswordUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
 
 @Service
 public class ClientService {
@@ -20,9 +24,10 @@ public class ClientService {
     private  ClientMapper clientMapper;
 
 
+
     public ClientResponseDTO createClient(ClientRequestDTO dto) {
         Client client = clientMapper.toEntity(dto);
-
+        client.setPassword(PasswordUtils.hash(dto.password()));
         return clientMapper.toDto(clientRepository.save(client));
     }
 }
