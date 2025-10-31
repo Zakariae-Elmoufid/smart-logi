397de3defe1a446082fc3c4a1d99679a719966ac
zakariae elmoufid <moufidzakarie3@gmail.com>
2025-10-31 10:11:31 +0100

    merge product into devlop

--- src/main/java/com/example/SmartLogi/dto/ManagerRequestDTO.java
+++ src/main/java/com/example/SmartLogi/dto/ManagerRequestDTO.java
@@ -1,4 +1,34 @@
 package com.example.SmartLogi.dto;
 
-public class ManagerRequestDTO {
-}
+import com.example.SmartLogi.validation.UniqueEmail;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.Builder;
+
+
+
+public record ManagerRequestDTO (
+        @NotBlank(message = "First name is required")
+                String firstName,
+
+        @NotBlank(message = "Last name is required")
+        String lastName,
+
+        @NotBlank(message = "Email is required")
+        @Email(message = "Email should be valid")
+        @UniqueEmail
+        String email,
+
+        @NotBlank(message = "Password is required")
+        @Size(min = 6, message = "Password must be at least 6 characters")
+        String password,
+
+        @NotBlank(message = "Phone number is required")
+        @Pattern(regexp = "\\+?\\d{10,15}", message = "Phone number must be numeric and 10-15 digits")
+        String phoneNumber
+
+) {}
+
+
