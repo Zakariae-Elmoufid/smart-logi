c6a0d2807dda2cf96e5ce87f9d30f2d51bb8a44e
zakariae elmoufid <moufidzakarie3@gmail.com>
2025-10-31 09:53:27 +0100

    updade entity warehouse and manager

--- src/main/java/com/example/SmartLogi/entities/Inventory.java
+++ src/main/java/com/example/SmartLogi/entities/Inventory.java
@@ -24,52 +24,5 @@ public class Inventory {
     @ManyToOne
     @JoinColumn(name = "warehouse_id")
     private Warehouse warehouse;
-    public Inventory() {}
-    public Inventory(Long id, Warehouse warehouse, Product product, int quantityReserved, int quantityOnHand) {
-        this.id = id;
-        this.warehouse = warehouse;
-        this.product = product;
-        this.quantityReserved = quantityReserved;
-        this.quantityOnHand = quantityOnHand;
-    }
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public int getQuantityOnHand() {
-        return quantityOnHand;
-    }
-
-    public void setQuantityOnHand(int quantityOnHand) {
-        this.quantityOnHand = quantityOnHand;
-    }
-
-    public int getQuantityReserved() {
-        return quantityReserved;
-    }
-
-    public void setQuantityReserved(int quantityReserved) {
-        this.quantityReserved = quantityReserved;
-    }
-
-    public Product getProduct() {
-        return product;
-    }
-
-    public void setProduct(Product product) {
-        this.product = product;
-    }
-
-    public Warehouse getWarehouse() {
-        return warehouse;
-    }
-
-    public void setWarehouse(Warehouse warehouse) {
-        this.warehouse = warehouse;
-    }
 }
--- src/main/java/com/example/SmartLogi/entities/WarehouseManager.java
+++ src/main/java/com/example/SmartLogi/entities/WarehouseManager.java
@@ -1,32 +1,53 @@
+package com.example.SmartLogi.entities;
+
+import com.example.SmartLogi.entities.Inventory;
 import com.example.SmartLogi.entities.Warehouse;
 import com.example.SmartLogi.enums.UserRole;
 import jakarta.persistence.Entity;
 import jakarta.persistence.*;
 import com.example.SmartLogi.entities.User;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
 
-//@Entity
-//@Table(name = "warehouse_manager")
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
+@Entity
+@Table(name = "warehouse_manager")
 public class WarehouseManager
-//        extends User
+        extends User
 {
 
 
-//    @OneToOne(fetch = FetchType.EAGER)
-//    @JoinColumn(name="warehouse_id", nullable = false)
-//    private Warehouse warehouse;
-//
-//    public WarehouseManager() {}
-//
-//    public WarehouseManager(String firstName, String lastName, String email, String password, UserRole role, Warehouse warehouse) {
-//        super(firstName, lastName, email,password,UserRole.WAREHOUSE_MANAGER);
-//        this.warehouse = warehouse;
-//    }
-//
-//    public Warehouse getWarehouse() {
-//        return warehouse;
-//    }
-//
-//    public void setWarehouse(Warehouse warehouse) {
-//        this.warehouse = warehouse;
-//    }
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(
+            name = "manager_warehouse",
+            joinColumns = @JoinColumn(name = "manager_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "warehouse_id", referencedColumnName = "id")
+    )
+    private Set<Warehouse> warehouses = new HashSet<>();
+
+    public void addWarehouse(Warehouse warehouse) {
+        this.warehouses.add(warehouse);
+        warehouse.getManagers().add(this);
+    }
+
+    public void removeWarehouse(Warehouse warehouse) {
+        this.warehouses.remove(warehouse);
+        warehouse.getManagers().remove(this);
+    }
+
+    // include id from superclass for equals/hashCode via Lombok explicit include
+    @EqualsAndHashCode.Include
+    private Long getIdentity() {
+        return this.getId();
+    }
+
+
 }
