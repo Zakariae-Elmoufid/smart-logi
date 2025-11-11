05367448a7ce0e41ce6f49ab81f7e20a5ba3a83c
zakariae elmoufid <moufidzakarie3@gmail.com>
2025-11-11 13:45:55 +0100

    create h2 for test db

--- src/main/java/com/example/SmartLogi/controllers/ProductController.java
+++ src/main/java/com/example/SmartLogi/controllers/ProductController.java
@@ -90,8 +90,4 @@ public class ProductController {
 
 
 
-
-
-
-
 }
--- src/test/java/com/example/SmartLogi/services/InventoryServiceTest.java
+++ src/test/java/com/example/SmartLogi/services/InventoryServiceTest.java
@@ -20,6 +20,8 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.time.LocalDateTime;
 import java.util.Optional;
@@ -29,6 +31,8 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
+@SpringBootTest
+@ActiveProfiles("test")
 @ExtendWith(MockitoExtension.class)
 public class InventoryServiceTest {
 
