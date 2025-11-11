5bbbfdb4f754955f47a83ff73928d47039ca447b
zakariae elmoufid <moufidzakarie3@gmail.com>
2025-11-11 14:27:22 +0100

    config h2

---
+++ src/main/java/com/example/SmartLogi/SmartLogiApplicationTests.java
@@ -0,0 +1,10 @@
+package com.example.SmartLogi;
+
+@SpringBootTest
+@ActiveProfiles("test")
+public class SmartLogiApplicationTests {
+    @Test
+    void contextLoads() {
+        System.out.println("contextLoads");
+    }
+}
